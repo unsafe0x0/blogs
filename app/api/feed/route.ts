680870b7bf0f26fcc028d4530f39c50bff7fb5a0
unsafe0x0 <priyannxhuu@gmail.com>
2025-09-30 18:09:52 +0530

    first commit

---
+++ app/api/feed/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import blogsData from "@/data/Data";
+
+export async function GET() {
+  return NextResponse.json(blogsData);
+}
---
+++ data/Data.tsx
@@ -0,0 +1,54 @@
+const blogsData = [
+  {
+    title: "Go Context Simplified",
+    description:
+      "A overview of Go's context package, its importance in managing timeouts and cancellations, and how to use it effectively.",
+    url: "https://blogs.priyanxhu.com/go-context",
+    slug: "go-context",
+    publishedAt: "2025-09-30",
+    readTime: "15 min read",
+    tags: ["Golang", "Programming", "Go", "Context", "Concurrency"],
+  },
+  {
+    title: "How to setup a HTTP server using Go and Gin",
+    description:
+      "An overview of how to setup a HTTP server using Go and Gin with some basic middleware.",
+    url: "https://blogs.priyanxhu.com/go-gin",
+    slug: "go-gin",
+    publishedAt: "2025-09-22",
+    readTime: "15 min read",
+    tags: ["Golang", "Programming", "Go", "Server", "Middleware", "Gin"],
+  },
+  {
+    title: "How Go(Golang) Works: Features, Workflow, and Use Cases",
+    description:
+      "An overview of how Go works, its key features, workflow, and common use cases.",
+    url: "https://blogs.priyanxhu.com/how-go-works",
+    slug: "how-go-works",
+    publishedAt: "2025-09-21",
+    readTime: "10 min read",
+    tags: ["Golang", "Programming", "Go", "Workflow", "Features"],
+  },
+  {
+    title: "MERN Stack Alternatives in Golang",
+    description:
+      "Exploring backend alternatives to the MERN stack using Golang libraries and frameworks for building robust server side applications.",
+    url: "https://blogs.priyanxhu.com/mern-alt",
+    slug: "mern-alt",
+    publishedAt: "2025-09-19",
+    readTime: "8 min read",
+    tags: ["Golang", "MERN", "Backend", "Web Development", "Alternatives"],
+  },
+  {
+    title: "Golang - A language with high testosterone",
+    description:
+      "An introduction to Go programming language, its features, and why it's great for building scalable systems.",
+    url: "https://blogs.priyanxhu.com/golang",
+    slug: "golang",
+    publishedAt: "2025-09-18",
+    readTime: "10 min read",
+    tags: ["Golang", "Programming", "Go", "Features", "Scalable"],
+  },
+];
+
+export default blogsData;
